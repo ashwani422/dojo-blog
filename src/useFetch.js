7525a15d82316e072f067e4f7d9cdbe8a4358e10
Ashwani <ashwanikumar150798@gmail.com>
2021-02-26 13:02:21 +0530

    Completed thenetninja tutorial

---
+++ src/useFetch.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = url => {
+
+  const [data, setData] = useState(null);
+  const [isPending, setPending] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+
+    const abortController = new AbortController();
+
+    fetch(url, { signal: abortController.signal })
+      .then(res => {
+        if (!res.ok) {
+          throw Error('Could not fetch the data for that resource.');
+        }
+        return res.json();
+      })
+      .then(data => {
+        setData(data);
+        setPending(false);
+        setError(null);
+      })
+      .catch(err => {
+        if (err.name === 'AbortError') {
+          console.log('fetch abort');
+        } else {
+          setPending(false);
+          setError(err.message);
+        }
+      });
+
+    return () => abortController.abort();
+  }, [url]);
+
+  return { data, isPending, error };
+}
+
+export default useFetch;
